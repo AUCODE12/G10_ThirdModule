10e05b9ab0f5ad94e475d1ab2e2ddbebe0042afe
Ahmadjon Qudratov <120676674+AUCODE12@users.noreply.github.com>
2025-02-04 19:20:46 +0500

    Update

---
+++ 3.10/MusicCRUD/MusicCRUD.DataAccess/EntityConfigurations/MusicConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicCRUD.DataAccess.Entity;
+
+namespace MusicCRUD.DataAccess.EntityConfigurations;
+
+public class MusicConfiguration : IEntityTypeConfiguration<Music>
+{
+    public void Configure(EntityTypeBuilder<Music> builder)
+    {
+        builder.ToTable("Music");
+
+        builder.HasKey(m => m.MusicId);
+
+        builder.Property(m => m.Name)
+            .HasMaxLength(30);
+
+        builder.Property(m => m.Description)
+            .HasMaxLength(50);
+
+        builder.Property(m => m.AuthorName) 
+            .HasMaxLength(20);
+
+        builder.HasMany(m => m.Comments)
+            .WithOne(m => m.Music)
+            .HasForeignKey(c => c.MusicId);
+            
+    }
+}
---
+++ 3.10/MusicCRUD/MusicCRUD.DataAccess/MainContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicCRUD.DataAccess.Entity;
+using MusicCRUD.DataAccess.EntityConfigurations;
+
+namespace MusicCRUD.DataAccess;
+
+public class MainContext : DbContext 
+{
+
+    public DbSet<Music> Music { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserDetail> UserDetails { get; set; }
+
+    public MainContext(DbContextOptions<MainContext> options)
+        : base(options)
+    {
+    }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    if (!optionsBuilder.IsConfigured)
+    //    {
+    //        optionsBuilder.UseSqlServer("Data Source=localhost\\SQLDEV;User ID=sa;Password=1;Initial Catalog=MusicCRUD;TrustServerCertificate=True;");
+    //    }
+    //}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new MusicConfiguration());
+    }
+}
